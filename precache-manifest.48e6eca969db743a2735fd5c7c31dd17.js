878d7dc60a841a5618898df62b134906ed6da05e
Omar Alhazmi <omaralhazmi@Omars-MacBook-Pro.local>
2021-08-11 20:50:36 +0300

    Updates

---
+++ precache-manifest.48e6eca969db743a2735fd5c7c31dd17.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b07cabe996aabc4d352831004cfe8f71",
+    "url": "/BOXage/index.html"
+  },
+  {
+    "revision": "f5fa44529b405dd1619b",
+    "url": "/BOXage/static/css/2.22ca87da.chunk.css"
+  },
+  {
+    "revision": "db3697f377ff7ae46b5a",
+    "url": "/BOXage/static/css/main.f3e55f2a.chunk.css"
+  },
+  {
+    "revision": "f5fa44529b405dd1619b",
+    "url": "/BOXage/static/js/2.66b0cd75.chunk.js"
+  },
+  {
+    "revision": "db3697f377ff7ae46b5a",
+    "url": "/BOXage/static/js/main.6df6f601.chunk.js"
+  },
+  {
+    "revision": "53ac6862f0c6f57c7e24",
+    "url": "/BOXage/static/js/runtime-main.fbcecb04.js"
+  },
+  {
+    "revision": "90dd671c242ef74f706b140b1826ac12",
+    "url": "/BOXage/static/media/add_c.90dd671c.svg"
+  },
+  {
+    "revision": "19d2da71fa14da96ba6136738c58852a",
+    "url": "/BOXage/static/media/add_to_cart.19d2da71.svg"
+  },
+  {
+    "revision": "1dc23fdbf19a5b4e61d9ab9e14ee12d9",
+    "url": "/BOXage/static/media/item.1dc23fdb.svg"
+  },
+  {
+    "revision": "dc0815b014d7f40c60e78ec6bb1bccb2",
+    "url": "/BOXage/static/media/shipping.dc0815b0.svg"
+  },
+  {
+    "revision": "887a8a803bfeb0a7d5883a84c8c3b347",
+    "url": "/BOXage/static/media/video.887a8a80.mp4"
+  }
+]);
